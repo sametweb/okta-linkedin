2fa2c75f9e41c25b74e162c296205b12cc721ddd
sametweb <sametmutevellioglu@gmail.com>
2020-06-03 14:58:06 -0700

    linkedin okta works

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Security, LoginCallback, useOktaAuth } from "@okta/okta-react";
-import axios from "axios";
 
 const config = {
   issuer: "https://dev-956984.okta.com/oauth2/default",
   clientId: "0oadqw2003tWfIV3J4x6",
   redirectUri: `${window.location.origin}/implicit/callback`,
-  pkce: false,
-  responseType: "id_token",
+  pkce: true,
+  responseType: "id_token token",
   responseMode: "fragment",
   scope: ["profile", "email", "openid"],
   state: "ANYVALUE",
@@ -28,15 +27,14 @@ const App = () => {
 
 const Home = () => {
   const { authState, authService } = useOktaAuth();
+  console.log({ authState, authService });
 
-  // console.log({ authService, authState });
-  authService.getUser().then((user) => console.log({ user }));
-
-  const login = async () => {
+  authService.getUser().then((res) => console.log(res));
+  const login = () => {
     authService.login("/"); // Redirect to '/' after login
   };
 
-  const logout = async () => {
+  const logout = () => {
     authService.logout("/"); // Redirect to '/' after logout
   };
 
@@ -50,7 +48,7 @@ const Home = () => {
     <>
       <button onClick={login}>Login</button>
       <a
-        href={`https://dev-956984.okta.com/oauth2/v1/authorize?idp=0oadr7cgkERe73AkA4x6&client_id=${config.clientId}&response_type=id_token&response_mode=fragment&scope=openid&redirect_uri=${window.location.origin}/implicit/callback&state=ANYVALUE&nonce=ANYVALUE`}
+        href={`https://dev-956984.okta.com/oauth2/v1/authorize?idp=0oadr7cgkERe73AkA4x6&client_id=${config.clientId}&response_type=id_token token&response_mode=fragment&scope=openid%20profile%20email&redirect_uri=${window.location.origin}/implicit/callback&state=ANYVALUE&nonce=ANYVALUE`}
       >
         Login with LinkedIn
       </a>
@@ -59,14 +57,30 @@ const Home = () => {
 };
 
 const Login = (props) => {
-  const token = props.location.hash.split("#id_token=")[1].split("&")[0];
+  // const token = props.location.hash.split("#id_token=")[1].split("&")[0];
+  const { authService, authState } = useOktaAuth();
+
+  useEffect(() => {
+    if (props.location.hash.includes("#id_token")) {
+      authService.login("/");
+    }
+  }, []);
 
   return (
-    <>
-      {token}
-      <LoginCallback />
-    </>
+    <div style={{ backgroundColor: "salmon" }}>
+      Thank you for logging in... Please wait, we are redirecting you to
+      homepage...
+      <br />
+      <div style={{ display: "none" }}>
+        <LoginCallback />
+      </div>
+    </div>
   );
 };
 
 export default App;
+
+// /linkedin.com -> Login ->
+// implicit/callback/#id_token=asdasdasdasdasd
+// implicit/callback
+// /
